c29ca65e895cc1473fffaeeb2d1917fcd3498c30
aben20807 <aben20807@gmail.com>
2016-03-27 11:03:08 +0800

    Rename, fix some bug, and add meaning rule

---
+++ Makefile
@@ -0,0 +1,4 @@
+studentID_meaning:Student.cpp studentID_meaning.cpp
+	g++ -o studentID_meaning Student.cpp studentID_meaning.cpp
+	
+
--- studentID_meaning.cpp
+++ studentID_meaning.cpp
@@ -8,9 +8,10 @@ int main()
     while(1)
     {
         cout<<"------------------------------"<<endl;
-        cout<<++count<<" : "<<endl<<"輸入學號 : ";
+        cout<<++count<<" : "<<endl;
+        cout<<"輸入學號 : ";
         cin>>id;
-        if(id=="00")break;
+        if(id=="00")break;//exit(1);
         Student stid(id);
         //cout<<stid.getStudentId()<<endl;
         cout<<"就讀科系 : "<<stid.getDepartment()<<endl;
@@ -18,7 +19,6 @@ int main()
         cout<<"入學年度 : "<<stid.getEntryYear()<<endl;
         cout<<"入學方式 : "<<stid.getEntryMethod()<<endl;
         cout<<"班級座號 : "<<stid.getNumber()<<endl<<endl;
-        
         if(stid.getCheck())
         {
             stid.setCheck(false);
